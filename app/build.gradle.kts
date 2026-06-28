5cd737f28d7627cd519e088a38b9b33fcf3644c4
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-06-28 17:17:10 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,22 +43,16 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
-    implementation("com.google.firebase:firebase-analytics")
-
     implementation("com.onesignal:OneSignal:[5.6.1, 5.9.99]")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    // Activity
+
     implementation("androidx.activity:activity-ktx:1.8.2")
-    // SwipeRefreshLayout
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    // Webkit
     implementation("androidx.webkit:webkit:1.10.0")
-    // SplashScreen
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     implementation("androidx.browser:browser:1.8.0")
