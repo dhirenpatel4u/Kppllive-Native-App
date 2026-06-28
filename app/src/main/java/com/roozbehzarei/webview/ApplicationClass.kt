1f4af58ca72c856feaeee6940a11fd2cd5918651
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-06-29 02:01:03 +0530

    Update ApplicationClass.kt

--- app/src/main/java/com/roozbehzarei/webview/ApplicationClass.kt
+++ app/src/main/java/com/roozbehzarei/webview/ApplicationClass.kt
@@ -11,6 +11,8 @@ import kotlinx.coroutines.launch
 
 import com.onesignal.OneSignal
 import com.onesignal.debug.LogLevel
+import android.content.Intent
+import org.json.JSONObject
 
 class ApplicationClass : Application() {
     override fun onCreate() {
@@ -21,6 +23,22 @@ class ApplicationClass : Application() {
 
         // Replace with your 36-character App ID from Dashboard > Settings > Keys & IDs
         OneSignal.initWithContext(this, "fa35403c-dda5-465c-a5b0-7a102ad0a9a0")
+
+        OneSignal.Notifications.addClickListener { event ->
+
+            val url = event.notification.additionalData
+                ?.optString("url")
+
+            val intent = Intent(this, MainActivity::class.java)
+
+            intent.putExtra("url", url)
+
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or
+                Intent.FLAG_ACTIVITY_SINGLE_TOP
+
+            startActivity(intent)
+        }
 
         // Prompt user for push notification permission
         // In production, consider using an in-app message instead for better opt-in rates
